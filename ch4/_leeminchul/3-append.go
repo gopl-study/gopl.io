82114ab409765a30d0144476bc731b2f9c6df875
LMC <2_minchul@naver.com>
2019-12-29 22:26:35 +0900

    update append
    - output 추가
    - else 구문 주석 추가

--- ch4/_leeminchul/3-append.go
+++ ch4/_leeminchul/3-append.go
@@ -7,7 +7,7 @@ func appendInt(x []int, y int) []int {
 	zlen := len(x) + 1
 	if zlen <= cap(x) { // 확장할 공간이 있음
 		z = x[:zlen]
-	} else {
+	} else { // 두배로 증가시킨 새 배열 할당
 		zcap := zlen
 		if zcap < 2*len(x) {
 			zcap = 2 * len(x)
@@ -27,3 +27,16 @@ func main() {
 		x = y
 	}
 }
+
+/* output
+0	cap=1	[0]
+1	cap=2	[0 1]
+2	cap=4	[0 1 2]
+3	cap=4	[0 1 2 3]
+4	cap=8	[0 1 2 3 4]
+5	cap=8	[0 1 2 3 4 5]
+6	cap=8	[0 1 2 3 4 5 6]
+7	cap=8	[0 1 2 3 4 5 6 7]
+8	cap=16	[0 1 2 3 4 5 6 7 8]
+9	cap=16	[0 1 2 3 4 5 6 7 8 9]
+*/
