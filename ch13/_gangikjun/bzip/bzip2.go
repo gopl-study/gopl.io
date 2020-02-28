229b31a3e0f3b7d65c4c84e006fc62b7b717d563
Limza <32995763+Limza@users.noreply.github.com>
2020-02-28 14:56:15 +0900

    ch13 (#69)
    * 4장 공부중
    
    * 학습중
    
    * 4장
    
    * ch5
    
    * ch5 fin
    
    * ch6 study
    
    * ch6 fin
    
    * ch7 study
    
    * ch7 fin
    
    * ch8 발표
    
    * ch8 study
    
    * ch9
    
    * ch10
    
    * ch11
    
    * ch12
    
    * ch12 study
    
    * ch12 fin
    
    * ch13 fin
    
    Co-authored-by: JEONG HYUN SEOK <nicewook@hotmail.com>

---
+++ ch13/_gangikjun/bzip/bzip2.go
@@ -0,0 +1,76 @@
+// Package bzip 는 bzip 압축(bzip.org)을 사용하는 writer를 제공한다
+package bzip
+
+/*
+#cgo CFLAGS: -IC:/Mingw-w64/mingw64/opt/include
+#cgo LDFLAGS: -LC:/Mingw-w64/mingw64/opt/lib -lbz2
+#include <bzlib.h>
+#include <stdlib.h>
+bz_stream* bz2alloc() { return calloc(1, sizeof(bz_stream)); }
+int bz2compress(bz_stream *s, int action,
+                char *in, unsigned *inlen, char *out, unsigned *outlen);
+void bz2free(bz_stream* s) { free(s); }
+*/
+import "C"
+import (
+	"io"
+	"unsafe"
+)
+
+type writer struct {
+	w      io.Writer // 내부 출력 스트림
+	stream *C.bz_stream
+	outbuf [64 * 1024]byte
+}
+
+// NewWriter 는 bzzip2로 압축된 스트림에 대한 writer를 반환한다.
+func NewWriter(out io.Writer) io.WriteCloser {
+	const blockSize = 9
+	const verbosity = 0
+	const workFactor = 30
+	w := &writer{w: out, stream: C.bz2alloc()}
+	C.BZ2_bzCompressInit(w.stream, blockSize, verbosity, workFactor)
+	return w
+}
+
+func (w *writer) Write(data []byte) (int, error) {
+	if w.stream == nil {
+		panic("closed")
+	}
+	var total int
+	for len(data) > 0 {
+		inlen, outlen := C.uint(len(data)), C.uint(cap(w.outbuf))
+		C.bz2compress(w.stream, C.BZ_RUN,
+			(*C.char)(unsafe.Pointer(&data[0])), &inlen,
+			(*C.char)(unsafe.Pointer(&w.outbuf)), &outlen)
+		total += int(inlen)
+		data = data[inlen:]
+		if _, err := w.w.Write(w.outbuf[:outlen]); err != nil {
+			return total, err
+		}
+	}
+	return total, nil
+}
+
+// Close 는 압축된 데이터를 비우고 스트림을 닫는다.
+func (w *writer) Close() error {
+	if w.stream == nil {
+		panic("closed")
+	}
+	defer func() {
+		C.BZ2_bzCompressEnd(w.stream)
+		C.bz2free(w.stream)
+		w.stream = nil
+	}()
+	for {
+		inlen, outlen := C.uint(0), C.uint(cap(w.outbuf))
+		r := C.bz2compress(w.stream, C.BZ_FINISH, nil, &inlen,
+			(*C.char)(unsafe.Pointer(&w.outbuf)), &outlen)
+		if _, err := w.w.Write(w.outbuf[:outlen]); err != nil {
+			return err
+		}
+		if r == C.BZ_STREAM_END {
+			return nil
+		}
+	}
+}
